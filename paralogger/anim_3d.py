1cbf4ee0c6e3d31b65c6b3622cb0b05539f4e8b7
fred_h <fredcab66@hotmail.com>
2019-10-17 18:24:35 +0200

    include everything in class visualizer

--- paralogger/anim_3d.py
+++ paralogger/anim_3d.py
@@ -22,23 +22,27 @@ mouv=  [[2,1,2,0,45, 45],
         [2,1,2,0,46, 52],
         [2,1,2,0,46, 53]]
 
-md = gl.MeshData.cylinder(rows=10, cols=20, radius=[2.0, 2.0], length=5.)
 
-m1 = gl.GLMeshItem(meshdata=md, smooth=False, drawFaces=False, drawEdges=True, edgeColor=(1,1,1,1)) 
+
+#m1 = gl.GLMeshItem(meshdata=md, smooth=False, drawFaces=False, drawEdges=True, edgeColor=(1,1,0,1)) 
 
 
 class Visualizer(object):
+
     def __init__(self):
         self.traces = dict()
         self.app = QtGui.QApplication(sys.argv)
         self.w = gl.GLViewWidget()
-        self.w.opts['distance'] = 40
-        self.w.setWindowTitle('pyqtgraph example: GLLinePlotItem')
+        self.w.opts['distance'] = 80
+        self.w.setWindowTitle('3D view track')
         self.w.setGeometry(0, 110, 1920, 1080)
         self.w.show()
 
         self.index = 0
 
+        self.md = gl.MeshData.cylinder(rows=10, cols=20, radius=[2.0, 2.0], length=5.)
+
+        self.geom = gl.GLMeshItem(meshdata=self.md, smooth=False, drawFaces=False, drawEdges=True, edgeColor=(1,1,0,1)) 
 
         # create the background grids
         gx = gl.GLGridItem()
@@ -56,16 +60,15 @@ class Visualizer(object):
         self.n = 15
 
        
-       
         #for i in range(self.n):
-        print("in vizuliaser :"+  str(0))
+        print("in vizuliaser")
 
         # m1.translate(mouv[0][0],mouv[0][1],mouv[0][2])
         # m1.rotate(mouv[0][3],0,1,0)
         # m1.rotate(mouv[0][4],1,0,0)
         # m1.rotate(mouv[0][5],0,0,1)
 
-        self.w.addItem(m1)
+        #self.w.addItem(m1)
 
     def start(self):
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
@@ -80,21 +83,20 @@ class Visualizer(object):
 
         i= self.index
         
-        #TODO  to try inclue m1 in self.m1
 
-        m1.resetTransform()
-        m1.translate(mouv[i][0],mouv[i][1],mouv[i][2])
-        m1.rotate(mouv[i][3],0,1,0)
-        m1.rotate(mouv[i][4],1,0,0)
-        m1.rotate(mouv[i][5],0,0,1)
+        self.geom.resetTransform()
+        self.geom.translate(mouv[i][0],mouv[i][1],mouv[i][2])
+        self.geom.rotate(mouv[i][3],0,1,0)
+        self.geom.rotate(mouv[i][4],1,0,0)
+        self.geom.rotate(mouv[i][5],0,0,1)
 
-        self.w.addItem(m1)
+        self.w.addItem(self.geom)
 
 
     def animation(self):
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update)
-        timer.start(20)
+        timer.start(30)
         self.start()
 
 
